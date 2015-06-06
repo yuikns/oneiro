395813fd04b7a15d5d9a78710b54838972541ee3
Nginx web server <nginx@bva0.argcv.com>
2015-06-06 04:27:15 -0400

    init

---
+++ header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<title>
+	<?php
+		global $page, $paged;
+		wp_title( '|', true, 'right' );
+		// Add the blog name.
+		bloginfo( 'name' );
+		// Add the blog description for the home/front page.
+		$site_description = get_bloginfo( 'description', 'display' );
+		if ( $site_description && ( is_home() || is_front_page() ) ) 
+			echo " | $site_description";
+		// Add a page number if necessary:
+		if ( $paged >= 2 || $page >= 2 ) 
+			echo ' | ' . sprintf( __( 'Page %s', 'Oneiro'), max( $paged, $page ) );
+	?></title>
+	
+
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<link rel="shortcut icon" href="/favicon.png" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="index" title="<?php bloginfo( 'name' ); ?>" href="<?php echo get_option('home'); ?>/" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
+	<link rel="alternate" type="application/atom+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'atom_url' ); ?>" />
+
+	<link rel="canonical" href="<?php echo curPageURL(); ?>" /> <!-- Canonical helps with SEO -->
+	<!-- Let IE8 compatible HTML5  -->
+		<!--[if lt IE 9]>
+			<script src="/wp-content/themes/Oneiro1.4.3/js/html5shiv.min.js"></script>
+		<![endif]-->
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    
+    <!-- <script  src="<?php bloginfo('template_url');?>/js/jquery.js"></script> -->
+ 	<?php
+		wp_deregister_script( 'jquery' );
+		wp_register_script( 'jquery', '//lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js', array(), '2.0.3');
+		wp_enqueue_script( 'jquery' );
+	?>
+  
+    
+    
+	<?php wp_head(); ?> 
+    
+    <?php if ( is_singular() ){ ?>
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/comments-ajax.js"></script>
+	<?php } ?>
+
+</head>
+
+<body>
+<!--Code generation start -->
+<div id="main"><!-- The bottom of the frame structures -->
+
+	<header>
+	<div id="header">
+        <div class="tcontainer">
+        
+         	<div class="bokeh">
+        			<?php  if (is_user_logged_in()) { ?> <!-- Control Panel -->
+                    <div id="if-logged-in">
+						<a href="<?php bloginfo('url'); ?>/wp-admin/">Control Panel</a>
+                    </div><!--#if-logged-in-->
+                    <?php } ?>
+                    
+                <div id="logo">   
+					<?php if( is_front_page() || is_home() ) { ?>
+						<h1><a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a></h1>
+					<?php } else { ?>
+						<h2><a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a></h2>
+					<?php } ?>
+				</div>
+            
+				<span style="display:block;overflow: hidden; width: 0; height: 0;"><?php bloginfo('description'); ?></span>
+			</div><!--.bokeh-->
+		</div><!--.container-->
+	</div>
+    </header>
+    
+	
+ 
+    <?php /* - Slides (if you are sure do not need to slide, delete the follow line */
+/*include(TEMPLATEPATH . '/slideshow.php');*/ ?>
+    
---
+++ sidebar.php
@@ -0,0 +1,73 @@
+<div id="sidebar">
+
+  <div id="sidebar-layout">
+	<?php if ( ! dynamic_sidebar( 'Sidebar' )) : ?>
+	<?php endif; ?>
+	<ul>
+    
+		<!--Random Posts-->
+		<li id="sidebar-random" class="widget">
+			<h3>Random Posts</h3>
+			<ul class="ulstyle">
+				<?php $rand_posts = get_posts("numberposts=5&orderby=rand"); foreach( $rand_posts as $post ) : ?>
+				<li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+				<?php endforeach; ?>
+			</ul>
+		</li>
+        
+        
+		<!--Recent Comments-->
+		<li id="sidebar-commt" class="widget">
+			<h3>Recent Comments</h3>
+			<ul class="ulstyle">
+			<?php
+					$limit_num = '8'; //Here is the comment limit
+					$my_email = "'" . get_bloginfo ('admin_email') . "'";//not show host's email
+					$rc_comms = $wpdb->get_results("
+					 SELECT ID, post_title, comment_ID, comment_author,comment_author_email, comment_content
+					 FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts
+					 ON ($wpdb->comments.comment_post_ID  = $wpdb->posts.ID)
+					 WHERE comment_approved = '1'
+					 AND comment_type = ''
+					 AND post_password = ''
+					 AND comment_author_email != $my_email
+					 ORDER BY comment_date_gmt
+					 DESC LIMIT $limit_num
+					 ");
+					$rc_comments = '';
+					foreach ($rc_comms as $rc_comm) { //get_avatar($rc_comm,$size='50')
+					$rc_comments .= "<li><a href='"
+					. get_permalink($rc_comm->ID) . "#comment-" . $rc_comm->comment_ID
+					//. htmlspecialchars(get_comment_link(  $rc_comm->comment_ID, array('type' => 'comment'))) // another implement of the previous line
+					. "' title='Commented on " . $rc_comm->post_title . ".'><span class='comer'>".$rc_comm->comment_author." : </span>".cut_str(strip_tags($rc_comm->comment_content),14)."</a></li>\n";
+					}
+					$rc_comments = convert_smilies($rc_comments);
+					echo $rc_comments;
+					?>	
+			</ul>
+		</li>  
+       
+     
+        
+		<!--Archives-->
+		<li id="sidebar-archives" class="widget">
+			<h3>Archives</h3>
+			<ul>
+				<?php wp_get_archives( 'type=monthly' ); ?>
+			</ul>
+		</li>
+        
+     <!--Links-->
+		<li id="sidebar-link" class="widget">
+			<h3>Links</h3>
+			<ul class="ulstyle">
+				 <?php wp_list_bookmarks('categorize=0&title_after=&title_before=&title_li='); ?>
+			</ul>
+		</li>
+         
+   </ul> 
+    
+  </div><!--sidebar-layout-->
+  <div style="height:60px;"></div>
+  <div class="clear"></div>
+</div><!--sidebar-->

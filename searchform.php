17666eaa05c9e6561004983c8d6d99844df54ff9
Yu Jing <yu@argcv.com>
2015-06-09 14:19:29 +0800

    search form update

--- searchform.php
+++ searchform.php
@@ -1,7 +1,8 @@
-<div id="search-bg"><span id="navulbghack"> </span>
+<div id="search-bg">
+<span id="navulbghack"> </span>
 <form method="get" id="searchform" class="search" action="<?php bloginfo('home');?>/">
-    <div><input name="s" type="text" id="s" onblur="if(this.value =='')this.value='Search...';this.className='search_text';" onfocus="this.value='';this.className='search_textfocus';" onclick="if(this.value=='Search...')this.value=''" value="Search..." class="search_text" />
-  <input type="submit" name="button" id="button" value="" class="search_submit" onmousemove="this.className='search_submithover'" onmousedown="this.className='search_submitactive'" onmouseout="this.className='search_submit'" /></div>
+  <div><input name="s" type="text" id="s" value="" class="search_text" placeholder="Search..." required />
+  <input type="submit" name="button" id="button" value="" class="search_submit" /></div>
 </form>
 
 </div>
